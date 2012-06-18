640cef3df75322087ac9e94326bf59d8656bb71c
Mike Ensor <mike@ensor.cc>
2012-06-18 11:13:15 -0500

    * Setting up project with some tests

--- src/main/java/cc/ensor/workshop/Thinggy.java
+++ src/main/java/cc/ensor/workshop/Thinggy.java
@@ -1,1 +1,27 @@
-public class Thinggy { }
+package cc.ensor.workshop;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Thinggy is just a POJO that does...nothing
+ */
+public class Thinggy {
+
+    private static final Logger LOG = LoggerFactory.getLogger(Thinggy.class);
+    private final String name;
+    private final String description;
+
+    public Thinggy(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
--- src/test/java/cc/ensor/workshop/ThinggyTest.java
+++ src/test/java/cc/ensor/workshop/ThinggyTest.java
@@ -1,1 +1,18 @@
-public class ThinggyTest { }
+package cc.ensor.workshop;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.startsWith;
+
+public class ThinggyTest {
+
+    @Test
+    public void thinggyTest() {
+        Thinggy thinggy = new Thinggy("Thinggy", "Some POJO for me to use");
+        assertThat(thinggy.getName(), is("Thinggy"));
+        assertThat(thinggy.getDescription(), startsWith("Some"));
+    }
+
+}
